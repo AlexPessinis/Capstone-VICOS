ed38ed5a06522a3a0310913694ea1e234d47949a
Alex Pessinis <alexjpessinis@gmail.com>
2026-08-11 09:59:52 -0400

    testing fixed bug in telemetry constructor

--- modules/telemetry.py
+++ modules/telemetry.py
@@ -31,8 +31,26 @@ class Telemetry:
     def __init__(self):
         print("Initializing OBD-II connection...")
 
-        self.connection = obd.OBD("/dev/ttyUSB0", baudrate=115200, fast=False)
+        # Force stable settings for clone adapters
+        self.connection = obd.OBD(
+            port="/dev/ttyUSB0",
+            baudrate=115200,
+            fast=False,
+            timeout=1,
+            protocol=obd.protocols.ISO_15765_4_CAN
+        )
+
         print("OBD Status:", self.connection.status())
+
+        # Manual initialization sequence (clone adapters need this)
+        self.connection.send("ATZ")
+        self.connection.send("ATE0")
+        self.connection.send("ATH1")
+        self.connection.send("ATS0")
+        self.connection.send("ATSP6")  # Force CAN 11-bit
+
+        print("RPM test:", self.connection.query(obd.commands.RPM))
+
 
 
 
