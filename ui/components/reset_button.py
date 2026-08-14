33bcea44a615333f84586eea34ead321d9582d85
Alex Pessinis <alexjpessinis@gmail.com>
2026-08-14 08:13:25 -0400

    modified reset button component visibility

--- ui/components/reset_button.py
+++ ui/components/reset_button.py
@@ -1,14 +1,13 @@
-# ui/components/reset_button.py
-
 from kivy.uix.button import Button
-
+from kivy.properties import ListProperty
 
 class ResetButton(Button):
+    background_color = ListProperty([0.8, 0.1, 0.1, 1])
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text = "RESET TRIP"
-        self.font_size = 48
-        self.background_normal = ""
-        self.background_color = (0.8, 0.1, 0.1, 1)
-        self.size_hint = (0.75, 0.75)
-        self.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        self.size_hint_y = None
+        self.height = 120
+        self.font_size = kwargs.get("font_size", 40)
+        self.color = (1, 1, 1, 1)
+        self.bold = True
